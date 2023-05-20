e490629930c9d4514e16adfd8d87fc8c4a21e1d1
mardi_buulolo123 <mardimbuulolo@gmail.com>
2023-05-20 16:10:23 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useRef } from "react";
+import { Grid } from "gridjs";
+import { Table } from "react-bootstrap";
 
 function App() {
+  useEffect(() => {
+    grid.render(wrapperRef.current);
+  });
+  const wrapperRef = useRef(null);
+  const grid = new Grid({
+    columns: ["Name", "Email", "Phone Number"],
+    search: true,
+    className: "table",
+    style: Table,
+
+    language: {
+      search: {
+        placeholder: "🔍 ketik kata kunci...",
+      },
+    },
+    data: [
+      ["John", "john@example.com", "(353) 01 222 3333"],
+      ["Mark", "mark@gmail.com", "(01) 22 888 4444"],
+      ["John", "john@example.com", "(353) 01 222 3333"],
+      ["Mark", "mark@gmail.com", "(01) 22 888 4444"],
+      ["John", "john@example.com", "(353) 01 222 3333"],
+      ["Mark", "mark@gmail.com", "(01) 22 888 4444"],
+    ],
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center" }}>
+      <div ref={wrapperRef} style={{ width: "40%" }} />
     </div>
   );
 }
